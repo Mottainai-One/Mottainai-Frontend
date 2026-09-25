33beb0e2417888945ef369402ee13fe803ea3c2b
João0524 <joaopedro.s.capelli@gmail.com>
2026-09-24 23:02:57 -0300

    fix: fix the images of auth flow

--- src/types/auth-layout.types.ts
+++ src/types/auth-layout.types.ts
@@ -3,5 +3,7 @@ import type { ReactNode } from 'react';
 export interface AuthLayoutProps {
   title: string;
   description: string;
+  mascotSrc: string;
+  mascotAlt: string;
   children: ReactNode;
 }
